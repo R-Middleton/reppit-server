35f67ddae49e60a38954c6c28a92ceb60f80120e
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-04-28 15:11:54 +0100

    initial commit setup Postgresql Mikro-orm & Graphql

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.start().then(() => {
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+      console.log('Server started on localhost:4000');
+    });
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
