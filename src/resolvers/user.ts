c933e84cdd7bea9c6c243c57f630222e47c11b45
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-04-30 14:16:03 +0100

    Authentication including forgot password complete

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -61,7 +61,8 @@ export class UserResolver {
       };
     }
 
-    const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+    const key = FORGET_PASSWORD_PREFIX + token;
+    const userId = await redis.get(key);
     if (!userId) {
       return {
         errors: [
@@ -88,6 +89,7 @@ export class UserResolver {
     user.password = await argon2.hash(newPassword);
     await em.persistAndFlush(user);
 
+    await redis.del(key);
     //log user in after resetting password
     req.session.userId = user._id;
     return { user };
